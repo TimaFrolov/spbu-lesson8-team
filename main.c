2682014937147cfc3eb913982f2593e73e43c71a
Dmitry Korepin <97905730+DreDDeR85@users.noreply.github.com>
2022-10-26 18:46:44 +0300

    Adding count of elements function

--- main.c
+++ main.c
@@ -12,17 +12,17 @@ typedef struct Tree
 
 Error makeEmptyTree(Tree **treeRef)
 {
-    
+
 }
 
-void freeTree(Tree *tree) 
+void freeTree(Tree *tree)
 {
 
 }
 
 Error exists(Tree *tree, Element element)
 {
-    
+
 }
 
 Error addElement(Tree *tree, Element element)
@@ -32,44 +32,38 @@ Error addElement(Tree *tree, Element element)
 
 Error addElements(Tree *tree, Element *elements, int count)
 {
-    
+
 }
 
-Error removeElement(Tree *tree, Element element) 
+Error removeElement(Tree *tree, Element element)
 {
-    
+
 }
 
 Error countOfElements(Tree *tree, int *countRef)
-{
-
-}
-
-Error treeHeight(Tree *tree, int *heightRef) 
-{
-    
-}
-
-Error printPreorder(Tree *tree) 
 {
     if (tree == NULL)
     {
         return 0;
     }
-    printf("%d", tree->element);
+    ++countRef;
     printPreorder(tree->leftChild);
     printPreorder(tree->rightChild);
-    return 0;
+    return *countRef;
+}
+
+Error treeHeight(Tree *tree, int *heightRef)
+{
+
+}
+
+Error printPreorder(Tree *tree)
+{
+
 }
 
 Error printInorder(Tree *tree)
 {
-    if (tree == NULL)
-    {
-        return -2;
-    }
-    printPreorder(tree->leftChild);
-    printf("%d", tree->element);
-    printPreorder(tree->rightChild);
-    return 0;
+
 }
+
