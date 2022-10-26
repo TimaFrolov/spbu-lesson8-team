d6a5cfa9d19eb736d15391e9d2a77bf4cc1a1ff1
Dmitry Korepin <97905730+DreDDeR85@users.noreply.github.com>
2022-10-26 18:38:36 +0300

    Adding 5 task solution

--- main.c
+++ main.c
@@ -10,27 +10,27 @@ typedef struct Tree
     struct Tree *rightChild;
 } Tree;
 
-Error makeEmptyTree(Tree **treeRef) 
+Tree *makeEmptyTree(void)
 {
-    
+    return NULL;
 }
 
 void freeTree(Tree *tree) 
 {
-    
+
 }
 
-Error exists(Tree *tree, Element element, bool *isExist) 
+Error exists(Tree *tree, Element element)
 {
     
 }
 
-Error addElement(Tree *tree, Element element) 
+Error addElement(Tree *tree, Element element)
 {
-    
+
 }
 
-Error addElements(Tree *tree, Element *elements, int count) 
+Error addElements(Tree *tree, Element *elements, int count)
 {
     
 }
@@ -40,9 +40,9 @@ Error removeElement(Tree *tree, Element element)
     
 }
 
-Error countOfElements(Tree *tree, int *countRef) 
+Error countOfElements(Tree *tree, int *countRef)
 {
-    
+
 }
 
 Error treeHeight(Tree *tree, int *heightRef) 
@@ -52,10 +52,24 @@ Error treeHeight(Tree *tree, int *heightRef)
 
 Error printPreorder(Tree *tree) 
 {
-    
+    if (tree == NULL)
+    {
+        return 0;
+    }
+    printf("%d", tree->element);
+    printPreorder(tree->leftChild);
+    printPreorder(tree->rightChild);
+    return 0;
 }
 
-Error printInorder(Tree *tree) 
+Error printInorder(Tree *tree)
 {
-    
+    if (tree == NULL)
+    {
+        return -2;
+    }
+    printPreorder(tree->leftChild);
+    printf("%d", tree->element);
+    printPreorder(tree->rightChild);
+    return 0;
 }
